2f67dafcd992186d1771222a511f46ee02e5b12a
MisterBeyer <97488203+MisterBeyer@users.noreply.github.com>
2023-02-11 14:39:37 -0800

    PID stuff

--- src/main/java/frc/robot/commands/Move10Feet.java
+++ src/main/java/frc/robot/commands/Move10Feet.java
@@ -1,68 +1,52 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import com.revrobotics.SparkMaxAlternateEncoder;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.Drivebase;
+import com.revrobotics.CANSparkMax;
 
-import com.revrobotics.*;
-import com.revrobotics.CANSparkMax.IdleMode;
-
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-/** An example command that uses an example subsystem.
- */
 public class Move10Feet extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  CANSparkMax front_Left;
-  Constants contant;
-  Drivebase drive;
-  CANSparkMax front_Right;
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public Move10Feet(Drivebase subsystem  ){
-    this.drive = subsystem;
-  } 
-   
-  
 
-  
+  private final Drivebase m_subsystem;
+
+  public Move10Feet(Drivebase subsystem) {
+   m_subsystem = subsystem;
+  }
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    System.out.println("Initialized");
-    drive.tankDrive(.3, .3);
+    m_subsystem.setEncoder();
   }
-  
-  
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drive.displayEncoder();
-    }
-  
+    //nick is so great
+
+      m_subsystem.tankDrive(.25, -.25);
+    
+    
+    System.out.println(" phil swift");
+    System.out.println("Encoder:" + m_subsystem.getEncoder());
+  }
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
-    drive.tankDrive(0, 0);
-    drive.setEncoder(); 
-
+    m_subsystem.tankDrive(0, 0);
+    m_subsystem.setIdleMode();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    
-    return drive.getEncoder() > 100;
+    return m_subsystem.getEncoder() > 110;
   }
 }
--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -10,31 +10,52 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import com.revrobotics.*;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 public class Drivebase extends SubsystemBase {
   Constants constant = new Constants();
     //Scanner scan = new Scanner(System.in);
     //System.out.println("HOW ARE YOU DOING TODAY");
     //int Answer = scan.nextInt();
+
     private final CANSparkMax front_Left = new CANSparkMax(1, MotorType.kBrushless);
     private final CANSparkMax back_Left = new CANSparkMax(2, MotorType.kBrushless);
     private final CANSparkMax front_Right = new CANSparkMax(4, MotorType.kBrushless);
     private final CANSparkMax back_Right = new CANSparkMax(3, MotorType.kBrushless);
+    private final PigeonIMU gyro = new PigeonIMU(10);
+   private PigeonIMU.FusionStatus fusionStatus = new PigeonIMU.FusionStatus();
 
 // TODO: create a speed limitor method that creates a speed limit verible that can be sent to example cammond
+public void setGyro(){
+  gyro.setFusedHeading(0);
+}
+public double getGyro(){
+  return gyro.getFusedHeading(fusionStatus);
+}
+public void displayGyro(){
+   System.out.println(gyro.getFusedHeading(fusionStatus));
+}
 public double getEncoder() {
    return front_Left.getEncoder().getPosition() * constant.inchesPerRev;
+   
   }
   public void setEncoder(){
     front_Left.getEncoder().setPosition(0);
     front_Right.getEncoder().setPosition(0);
 
-  }    
+  }   
+  
+  public void setIdleMode(){
+    front_Left.setIdleMode(IdleMode.kBrake);
+    front_Right.setIdleMode(IdleMode.kBrake);
+  }
   
   public void displayEncoder() {
     System.out.println(front_Left.getEncoder().getPosition());
   }
     public void tankDrive(double left_Input, double right_Input) {
-        
+        getGyro();
        front_Left.set(left_Input );
         front_Right.set(right_Input );
 
